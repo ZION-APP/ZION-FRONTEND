12bc86a1eb6b6f6cab77024cda171bb33012399d
David Mendoza <drmendoz@espol.edu.ec>
2021-06-13 23:12:58 -0500

    Diseno de pantalla mas opciones corregido

---
+++ lib/screens/menu/components.dart/opcion.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:zionApp/Constants.dart';
+import 'package:zionApp/sizeConfig.dart';
+
+class OptionMenuItem extends StatelessWidget {
+  String titulo;
+  Widget widget;
+  OptionMenuItem({this.titulo, this.widget});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: kDisableColor),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: getProportionateScreenWidth(5),
+              horizontal: getProportionateScreenWidth(8)),
+          child: Row(
+            children: [
+              Flexible(
+                flex: 10,
+                child: Text(titulo),
+                fit: FlexFit.tight,
+              ),
+              Flexible(
+                flex: 1,
+                child: Icon(Icons.arrow_forward_ios_outlined),
+                fit: FlexFit.tight,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/menu/menu.dart
+++ lib/screens/menu/menu.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:zionApp/Constants.dart';
+import 'package:zionApp/screens/menu/components.dart/botonCerrarSesion.dart';
+import 'package:zionApp/screens/menu/components.dart/opcion.dart';
+import 'package:zionApp/sizeConfig.dart';
 
 class MenuHome extends StatefulWidget {
   @override
@@ -8,8 +12,80 @@ class MenuHome extends StatefulWidget {
 class _MenuHomeState extends State<MenuHome> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Menu"),
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenHeight(14),
+          vertical: getProportionateScreenWidth(30)),
+      child: Container(
+        child: Column(
+          children: [
+            _submenuZion(),
+            SizedBox(
+              height: getProportionateScreenHeight(35),
+            ),
+            _submenuCuenta(),
+            SizedBox(
+              height: getProportionateScreenHeight(50),
+            ),
+            LogOutButton()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _submenuZion() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "ZION",
+          style: TextStyle(
+              color: kDisableColor, fontSize: getProportionateScreenHeight(13)),
+        ),
+        Column(
+          children: [
+            OptionMenuItem(
+              titulo: "Quiénes Somos",
+            ),
+            OptionMenuItem(
+              titulo: "Preguntas frecuentes",
+            ),
+            OptionMenuItem(
+              titulo: "Contáctanos",
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Column _submenuCuenta() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "CUENTA",
+          style: TextStyle(
+              color: kDisableColor, fontSize: getProportionateScreenHeight(13)),
+        ),
+        Column(
+          children: [
+            OptionMenuItem(
+              titulo: "Mi Perfil",
+            ),
+            OptionMenuItem(
+              titulo: "Cuenta Bancaria",
+            ),
+            OptionMenuItem(
+              titulo: "Notificaciones",
+            ),
+            OptionMenuItem(
+              titulo: "Términos de Servicio",
+            )
+          ],
+        )
+      ],
     );
   }
 }
