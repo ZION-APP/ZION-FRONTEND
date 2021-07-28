68157f76216c0a4b6ddf6b07a44fea87ddf2f329
xecarlier <xecarlie@espol.edu.ec>
2021-07-28 11:18:38 -0500

    pantalla inicial

--- lib/screens/inversiones/inversiones.dart
+++ lib/screens/inversiones/inversiones.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:zionapp/components/button_default.dart';
+import 'package:zionapp/constants_config.dart';
+import 'package:zionapp/size_config.dart';
 
 class InversionesHome extends StatefulWidget {
   @override
@@ -8,6 +11,144 @@ class InversionesHome extends StatefulWidget {
 class _InversionesState extends State<InversionesHome> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Inversiones");
+    return SizedBox(
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                SizedBox(height: getProportionateScreenHeight(40)),
+                const Text(
+                  'Seleccione un fondo a invertir',
+                  style: TextStyle(fontSize: 21),
+                ),
+                SizedBox(height: getProportionateScreenHeight(40)),
+                SizedBox(
+                  width: 350,
+                  child: Column(
+                    children: [
+                      Image.asset("assets/img/inversion_alpha_banner.PNG"),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: kSecondaryColor, width: 4)
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(height: getProportionateScreenWidth(20),),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    SizedBox(height: getProportionateScreenHeight(5)),
+                                    const Text(
+                                      'Rendimiento promedio: 4.91%',
+                                      style: TextStyle(
+                                        color: kSecondaryColor,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                    SizedBox(height: getProportionateScreenHeight(5)),
+                                    const Text(
+                                      'Permanencia mínima: 32 días',
+                                      style: TextStyle(
+                                        color: kSecondaryColor,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                    SizedBox(height: getProportionateScreenHeight(5)),
+                                    const Text(
+                                      'Inversión inicial: \$100',
+                                      style: TextStyle(
+                                        color: kSecondaryColor,
+                                        fontSize: 17,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(width: getProportionateScreenWidth(10),),
+                                Image.asset('assets/img/certificacion_zion.PNG')
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: getProportionateScreenHeight(30)),
+                              child: DefaultButton(
+                                func: () => {},
+                                label: "Invertir en este fondo",
+                                colorFondo: kPrimaryColor,
+                                colorTexto: kSecondaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(40),),
+                      Image.asset("assets/img/inversion_omega_banner.PNG"),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: kSecondaryColor, width: 4)
+                        ),
+                        child: Column(
+                          children: [
+                            SizedBox(height: getProportionateScreenWidth(20),),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Column(
+                                  children: [
+                                    SizedBox(height: getProportionateScreenHeight(5)),
+                                    const Text(
+                                      'Rendimiento promedio: 6.08%',
+                                      style: TextStyle(
+                                        color: kSecondaryColor,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                    SizedBox(height: getProportionateScreenHeight(5)),
+                                    const Text(
+                                      'Permanencia mínima: 365 días',
+                                      style: TextStyle(
+                                        color: kSecondaryColor,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                    SizedBox(height: getProportionateScreenHeight(5)),
+                                    const Text(
+                                      'Inversión inicial: \$100',
+                                      style: TextStyle(
+                                        color: kSecondaryColor,
+                                        fontSize: 17,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(width: getProportionateScreenWidth(10),),
+                                Image.asset('assets/img/certificacion_zion.PNG')
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: getProportionateScreenHeight(30)),
+                              child: DefaultButton(
+                                func: () => {},
+                                label: "Invertir en este fondo",
+                                colorFondo: kPrimaryColor,
+                                colorTexto: kSecondaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          )
+        ],
+      ),
+    );
   }
 }
